4be1e072317cff82d9acbc489aef4171e73c1aea
skrth00 <skrth00@naver.com>
2017-01-15 01:45:09 +0900

    서버 작업중

--- TicSong_Media/RankingTableViewController.swift
+++ TicSong_Media/RankingTableViewController.swift
@@ -12,7 +12,8 @@ import Alamofire
 class RankingTableViewController: UITableViewController {
     
     var rankings = [Ranking]()
-    var countRanking :Int = 0
+    var countRanking :Int = 30
+    var userId : String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +25,21 @@ class RankingTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
         
+//        
+//        if let savedRankings = loadRankings() {
+//            rankings += savedRankings
+//            // += 대신에 = 해도 되지않을까...
+//        }
+   
+        let user = UserDefaults.standard
         
-        if let savedRankings = loadRankings() {
-            rankings += savedRankings
-            // += 대신에 = 해도 되지않을까...
-        }
+        userId = (user.stringArray(forKey:"user")?[1])!
+       
+        //print(rankings[0].name)
+    
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -54,13 +65,18 @@ class RankingTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        print("여기....\(indexPath.row)")
+        
+        rankings.removeAll()
+        selectRankings(userId: userId)
+
         // Table view cells are reused and should be dequeued using a cell identifier.
         let cellIdentifier = "RankingTableViewCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RankingTableViewCell
         
         // Fetches the appropriate meal for the data source layout.
         let ranking = rankings[indexPath.row]
-        
+        print(ranking)
     
         cell.numberLabel.text = ranking.number
         cell.nameLabel.text = ranking.name
@@ -72,31 +88,36 @@ class RankingTableViewController: UITableViewController {
     
     
     
-     func loadRankings() -> [Ranking]? {
-        
-        //서버에서 불러다가 [Ranking] type으로 반환하면 됨.
-        
-        
-        
-        // as? [Ranking] return
-        return nil
-    }
+//     func loadRankings() -> [Ranking]? {
+//        
+//        //서버에서 불러다가 [Ranking] type으로 반환하면 됨.
+//        
+//        
+//        
+//        // as? [Ranking] return
+//        return nil
+//    }
     
     
-    func selectRankings(userId:String, exp:Int, userLevel:Int, item1Cnt:Int, item2Cnt:Int, item3Cnt:Int, item4Cnt:Int)  {
-        
-        
-        let baseURL = "http://52.79.152.130/TicSongServer/item.do"
-        
-        let parameters: Parameters = ["service":"update", "userId":userId, "item1Cnt":item1Cnt, "item2Cnt":item2Cnt,"item3Cnt":item3Cnt, "item4Cnt":item4Cnt]
-        
-        
-        Alamofire.request(baseURL,method : .post, parameters: parameters, encoding: URLEncoding.default, headers: nil)
+    func selectRankings(userId:String)  {
+        print("selectRanking 호출")
+        let baseURL = "http://52.79.152.130/TicSongServer/myscore.do"
+        print("selectRanking 호출2")
+
+        let parameters: Parameters = ["service":"rank","userId":userId]
+        print(parameters)
+        print("selectRanking 호출3")
+
+        Alamofire.request(baseURL,method : .get, parameters: parameters, encoding: URLEncoding.default, headers: nil)
             .responseJSON { (response:DataResponse<Any>) in
-                
+                print("selectRanking 호출3-1")
+
                 switch(response.result) {
                 case .success(_):
                     if response.result.value != nil{
+                        
+                        print("selectRanking 호출4")
+
                         //print(response.result.value!)
                         
                         if let data = response.data, let utf8Text = String(data: data, encoding: .utf8), let encodedData = utf8Text.data(using: String.Encoding.utf8){
@@ -104,17 +125,60 @@ class RankingTableViewController: UITableViewController {
                             let JSON = try! JSONSerialization.jsonObject(with: encodedData, options: [])
                             
                             if let JSON = JSON as? [String: AnyObject] {
-                                if let resultCode = JSON["resultCode"] as? Int {
-                                    
-                                    if(resultCode == 1){
-                                        print("성공적으로 exp와 userLevel update!")
-                                    }else{print("exp와 userLevel update 실패…")
+                                
+                                if let resultCode = JSON["resultCode"] as? String{
+                                    print("selectRanking 호출5")
+
+                                    if(resultCode == "1"){
+                                        
+                                        var ranking: Ranking!
+                                        if let result = JSON["rankerList"] as? [AnyObject]{
+                                            
+                                            let counts =  result.map(
+                                                {(a: AnyObject) -> Int in
+                                                    print("여기 호출")
+                                                    let rankDict = a as! Dictionary<String, AnyObject>
+                                                    
+                                                    let rank = rankDict["rank"] as! Int
+                                                    let name = rankDict["name"] as! String!
+                                                    let exp = rankDict["exp"] as! Int
+                                                    let userLevel = rankDict["userLevel"] as! Int
+                                                    
+                                                    
+                                                    
+                                                    ranking = Ranking(number: String(rank), name: name!, exp : String(exp), level : String(userLevel))
+                                                    
+                                                    self.rankings += [ranking]
+                                                    
+                                                    print(ranking)
+                                                
+                                                    
+                                                    return a.count
+                                                }
+                                            )
+                                            self.countRanking = counts.count
+                                            
+                                            print("counts: \(counts.count)")
+                                            print(self.rankings[0].name)
+                                            
+                                            
+                                        }
+                                        
+                                        if let myrank = JSON["myRank"] as? Int{
+                                            
+                                            print("myrank: \(myrank)")
+                                        }
+                                        
+                                        
+                                        
                                     }
-                                    
-                                    
                                 }
+                                
+                                
                             }}
-                        //self.performSegue(withIdentifier: "LoginToMainSegue", sender: self)
+                        
+                        
+                        
                     }
                     break
                     
@@ -127,6 +191,17 @@ class RankingTableViewController: UITableViewController {
         }
         
         
+        }
+        
+    
+    
+    
+    @IBAction func toMain(_ sender: UIBarButtonItem) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
     }
 
     /*
@@ -179,4 +254,4 @@ class RankingTableViewController: UITableViewController {
    
 
 
-}
+
